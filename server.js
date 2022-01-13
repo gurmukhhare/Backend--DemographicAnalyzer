e8641fb1df8b630a937bf3d10e179015abada82f
Gurmukh Hare <gurmukhhare@Gurmukhs-MacBook-Pro.local>
2022-01-12 16:50:25 -0800

    fixing indentation issues on code editor

--- server.js
+++ server.js
@@ -21,27 +21,27 @@ app.use(cors());
 
 
 app.get('/',(req,res)=>{
-	res.json('this is working');
+    res.json('this is working');
 })
 
 /**
 * Sign-in endpoint, checks inputted email and password with saved hash in db. Returns error if incorrect credentials
 */
 app.post('/signin',(req,res)=>{
-	db.select('email', 'hash').from('login')
-	.where('email','=',req.body.email)
-	.then(data =>{
-		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-		if(isValid){
-			return db.select('*').from('users')
-			.where('email','=',req.body.email)
-			.then(user =>{
-				res.json(user[0])
-			}).catch(err => res.status(400).json('unable to get user'))
-		} else{
-		res.status(400).json('wrong credentials')
-		}
-	}).catch(err => res.status(400).json('wrong credentials'))
+    db.select('email', 'hash').from('login')
+    .where('email','=',req.body.email)
+    .then(data =>{
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if(isValid){
+            return db.select('*').from('users')
+            .where('email','=',req.body.email)
+            .then(user =>{
+                res.json(user[0])
+            }).catch(err => res.status(400).json('unable to get user'))
+        } else{
+        res.status(400).json('wrong credentials')
+        }
+    }).catch(err => res.status(400).json('wrong credentials'))
 
 
 })
@@ -51,25 +51,25 @@ app.post('/signin',(req,res)=>{
 * Register new user. Handled as a single db transaction
 */
 app.post('/register',(req,res)=>{
-	const { email,name,password }=req.body;
-	const hash = bcrypt.hashSync(password);
-		db.transaction(trx => {
-			trx.insert({
-				hash: hash,
-				email: email
-			})
-			.into('login')
-			.returning('email')
-			.then(loginEmail =>{
-				return trx('users')
-				.returning('*')
-				.insert({
-					email: loginEmail[0],
-					name: name,
-					joined: new Date ()
-				}).then(user =>{res.json(user[0]);})
-			}).then(trx.commit).catch(trx.rollback)
-		}).catch(err => res.status(400).json('unable to register'))
+    const { email,name,password }=req.body;
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail =>{
+                return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date ()
+                }).then(user =>{res.json(user[0]);})
+            }).then(trx.commit).catch(trx.rollback)
+        }).catch(err => res.status(400).json('unable to register'))
 })
 
 
@@ -77,39 +77,39 @@ app.post('/register',(req,res)=>{
 * Retrieve user information based on ID URL parameter
 */
 app.get('/profile/:id',(req,res) => {
-	const { id }=req.params;   /*parameters is an object containing 
-	parameters values parsed from the url path. if we use :'' syntax in url, we can grab the parameter 
-	using req.params*/
-	db.select('*').from('users').where({
-		id: id
-	}).then(user =>{
-		if(user.length){
-			res.json(user[0])
-		} else{
-			res.status(400).json('not found')
-		}
-	})
-	.catch(err => res.status(400).json('error getting user'))
-	}) 
+    const { id }=req.params;   /*parameters is an object containing 
+    parameters values parsed from the url path. if we use :'' syntax in url, we can grab the parameter 
+    using req.params*/
+    db.select('*').from('users').where({
+        id: id
+    }).then(user =>{
+        if(user.length){
+            res.json(user[0])
+        } else{
+            res.status(400).json('not found')
+        }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+    }) 
 
 
 /**
 * Update user entries, increment by 1 for each use
 */
 app.put('/image',(req,res)=>{
-	const { id }=req.body;   /*parameters is an object containing 
-	parameters values parsed from the url path. if we use :'' syntax in url, we can grab the parameter 
-	using req.params*/
-	db('users').where('id', '=', id)
-  	.increment('entries',1).returning('entries').then(entries =>{
-  		res.json(entries[0]);
-  	})
-  	.catch(err => res.status(400).json('cannot get entries'))
+    const { id }=req.body;   /*parameters is an object containing 
+    parameters values parsed from the url path. if we use :'' syntax in url, we can grab the parameter 
+    using req.params*/
+    db('users').where('id', '=', id)
+    .increment('entries',1).returning('entries').then(entries =>{
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('cannot get entries'))
 })
 
 
 app.listen(3000, ()=>{
-	console.log('app is running');
+    console.log('app is running');
 });
 
 
